e597ea5485bedba85b46cbc12772d9810a215a09
haizai <haizainaive@gmail.com>
2016-11-05 17:09:51 +0800

    add baidu-tongji

---
+++ server/public/js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?cf1464c8d4e9962b7f7bd2660de64e39";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
