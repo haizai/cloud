e68dababee87dc87d7d297a62cbc06f07f9558e0
haizai <haizainaive@gmail.com>
2016-11-09 11:53:46 +0800

    var title = title || null

--- server/routes/file.js
+++ server/routes/file.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/file', function(req, res, next) {
-  res.render('file',{index:'file'});
+  res.render('file');
 });
 
 module.exports = router;
