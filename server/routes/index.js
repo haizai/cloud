12db20a85b9798d18207f295d26fc9d4e09817b1
haizai <haizainaive@gmail.com>
2016-11-19 16:47:01 +0800

    render mobile

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,14 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-
   var deviceAgent = req.headers['user-agent'].toLowerCase();
   var agentID = deviceAgent.match(/(iphone|ipod|ipad|android)/);
-  if(agentID){
-    console.log('mobile', deviceAgent)
+  if(agentID) {
     res.redirect('/mobile')
-  }else{
-    console.log('px', deviceAgent)
+  } else {
    res.render('index', {title:'index'});
   }
 });
