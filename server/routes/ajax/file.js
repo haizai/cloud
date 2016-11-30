d7806dc47c0314a5c9239270c9e30d27077f8552
haizai <haizainaive@gmail.com>
2016-11-30 14:14:58 +0800

    close file server

--- server/routes/ajax/file.js
+++ server/routes/ajax/file.js
@@ -9,29 +9,31 @@ var router = express.Router();
 
 router.post('/file', (req, res) => {
 
-	if (req.query.password != password) {
-		res.send('error! \n\n password false')
-		return
-	}
+	// if (req.query.password != password) {
+	// 	res.send('error! \n\n password false')
+	// 	return
+	// }
 
-	var form = new formidable.IncomingForm();
-	form.encoding = 'utf-8';
-	form.keepExtensions = true;
-	form.uploadDir = path.resolve(__dirname,'..','..','..','upload/')
+	// var form = new formidable.IncomingForm();
+	// form.encoding = 'utf-8';
+	// form.keepExtensions = true;
+	// form.uploadDir = path.resolve(__dirname,'..','..','..','upload/')
 
-	form.parse(req, (err, fields, files) => {
-		if (!files.file || files.file.size === 0) {
-			res.send('error! \n\n no file')
-		  return
-		}
-    var dir = path.normalize(files.file.path)
-    fs.exists(dir, exists=>{
-    	if (exists)
-   		fs.rename(dir, path.join(path.dirname(dir), files.file.name))
-    })
-    res.send('success! \n\n'+util.inspect({fields, files}))
-  });
-	return
+	// form.parse(req, (err, fields, files) => {
+	// 	if (!files.file || files.file.size === 0) {
+	// 		res.send('error! \n\n no file')
+	// 	  return
+	// 	}
+ //    var dir = path.normalize(files.file.path)
+ //    fs.exists(dir, exists=>{
+ //    	if (exists)
+ //   		fs.rename(dir, path.join(path.dirname(dir), files.file.name))
+ //    })
+ //    res.send('success! \n\n'+util.inspect({fields, files}))
+ //  });
+	// return
 
+
+  res.send('error! \n\n file server is closed')
 })
 module.exports = router;
