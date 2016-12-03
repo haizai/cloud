26b95ffb0427f520bf2ed8e00c2b2b7367c50088
haizai <haizainaive@gmail.com>
2016-12-03 14:55:45 +0800

    change index <img> to background-image

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,23 +1,63 @@
 jQuery(function($){
 
+  // 禁止拖动
   document.body.onmousedown = function() {
     return false
   }
 
+
   var bH = $('body').height()
-  $(window).resize(function(e){
-    if (bH !== $('body').height()) {
-      bH = $('body').height()
-      setItemHeight()
-      $('body').scrollTop(bH*sel)
-    }
-  });
   var sel = 0
   var len = $('.part').length
   var onMove = false
 
-  setItemHeight()
+  //出现侧栏
+  for (var i = 0; i < len; i++) {
+    if (i==0) {
+      $('<div class="slide-item slide-item-in"></div>').appendTo($('.slide'))
+    } else {
+      $('<div class="slide-item"></div>').appendTo($('.slide'))
+    }
+    
+  }
 
+
+  // 读取背景  
+  var Img = {
+    smallSrcs: [
+      "../img/index/p1_s.jpg",
+      "../img/index/p2_s.jpg",
+      "../img/index/p0_s.jpg",
+      "../img/index/p3_s.jpg",
+      "../img/index/p4_s.jpg"
+    ],
+    bigSrcs: [
+      "../img/index/p1.jpg",
+      "../img/index/p2.png",
+      "../img/index/p0.png",
+      "../img/index/p3.jpg",
+      "../img/index/p4.jpg"
+    ]
+  }
+  loadImg(0, false)
+  function loadImg(index, isBig) {
+    var newImg = new Image()
+    newImg.src = isBig ? Img.bigSrcs[index] :Img.smallSrcs[index]
+    newImg.onload = function() {
+      $('.part').eq(index).css({'backgroundImage': "url('" + newImg.src + "')"})
+      if (!isBig) {
+        index < len - 1 ? loadImg(++index, false) : loadImg(0, true)
+      } else {
+        if (index < len - 1) {
+          loadImg(++index, true)
+        }
+      }
+    }
+  }
+
+
+  // 设置高度
+  setItemHeight()
   function setItemHeight() {
     $('.part').each(function(){
       $(this).css({
@@ -25,13 +65,19 @@ jQuery(function($){
       })
     })
   }
-  for (var i = 0; i < len; i++) {
-    if (i==0) {
-      $('<div class="slide-item slide-item-in"></div>').appendTo($('.slide'))
-    } else {
-      $('<div class="slide-item"></div>').appendTo($('.slide'))
+ 
+  // 窗口大小重置 
+  $(window).resize(function(e){
+    if (bH !== $('body').height()) {
+      bH = $('body').height()
+      setItemHeight()
+      $('body').scrollTop(bH*sel)
     }
-  }
+  });
+
+
+
+  // 侧栏点击
   $('.slide-item').each(function(index){
     $(this).on('click', function() {
       sel = index
@@ -41,6 +87,7 @@ jQuery(function($){
 
   moveTo(0, true)
 
+  // 核心移动方法
   function moveTo(index, isInit) {
     // console.log('moveTo',bH,index)
     onMove = true
@@ -49,7 +96,7 @@ jQuery(function($){
     })
     $('html,body').animate({scrollTop : bH*index},isInit?10:500,function(){
         onMove = false,
-        $('.part').eq(index).find('.part-text').animate({opacity: 1,marginRight: '30px'},500,function(){
+        $('.part').eq(index).find('.part-tips').animate({opacity: 1,marginRight: '50px'},500,function(){
       })
     })
     $('.slide-item').each(function(i){
@@ -62,6 +109,7 @@ jQuery(function($){
 
   }
 
+  // 滚轮滚动
   $(document).on('mousewheel',function (e) {
     if (!onMove) {
       if(sel == 0) {
@@ -81,13 +129,14 @@ jQuery(function($){
     }
   })
 
+  // 按下移动
   $('.part').on('mousedown', function(e1) {
     if (!onMove) {
       var startY = e1.clientY
       var $this = $(this)
       $this.on('mousemove', function(e2) { 
         // console.log(e2.clientY-startY)
-        if (e2.clientY-startY < -15) {
+        if (e2.clientY-startY < -10) {
           if (sel < len-1) {
             sel++
             moveTo(sel)
@@ -95,7 +144,7 @@ jQuery(function($){
             $this.off('mouseup')
           }
         }
-        if (e2.clientY-startY > 15) {
+        if (e2.clientY-startY > 10) {
           if (sel > 0) {
             sel--
             moveTo(sel)
@@ -109,11 +158,6 @@ jQuery(function($){
         $this.off('mouseup')
       })
     }
-  })
-
-  $('.part-img-big').on('load', function(){
-    $(this).siblings('.part-img-small').css({display:'none'})
-    $(this).css({display:'block'})
   })
 
 
