baa6f2a2dee754d293cb1025d0347e7742c49b01
haizai <haizainaive@gmail.com>
2016-12-07 14:36:55 +0800

    add console 访问者  ifname address time

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var util = require('util')
+var os = require('os');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
   var deviceAgent = req.headers['user-agent'].toLowerCase();
   var agentID = deviceAgent.match(/(iphone|ipod|ipad|android)/);
+
+  var ifaces = os.networkInterfaces();
+
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+    ifaces[ifname].forEach(function (iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        return;
+      }
+      if (alias >= 1) {
+        console.log(util.inspect({ifname, alias,address:iface.address, time: req._startTime},{colors: true}));
+      } else {
+        console.log(util.inspect({ifname, address: iface.address, time: req._startTime},{colors: true}));
+      }
+      ++alias;
+    });
+  });
+
+
   if(agentID) {
     res.redirect('/mobile')
   } else {
