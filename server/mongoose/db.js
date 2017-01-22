733b070ae1b9ecf71a315c26affd2dc5cb77a985
haizai <haizainaive@gmail.com>
2017-01-22 16:32:13 +0800

    允许不开启mongoose下运行

--- server/mongoose/db.js
+++ server/mongoose/db.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
 
-var db = mongoose.connect('mongodb://localhost/haizai')
-
-module.exports = db;
+mongoose.connect('mongodb://localhost/haizai', (err,db)=>{
+  if (err) console.log('server/mongoose/db.js ERROR: \n' ,err , '\n')
+  module.exports = db;
+})
