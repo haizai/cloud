db3bb51ecc98e95e4da96b771a88702d9f734709
haizai <haizainaive@gmail.com>
2017-01-22 18:27:39 +0800

    add route login

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,11 +4,13 @@ var router = express.Router();
 var file = require('./file')
 var animes = require('./animes')
 var index = require('./index')
+var login = require('./login')
 var ajax = require('./ajax/index')
 var mobile = require('./mobile/index')
 
 router.use(file)
 router.use(animes)
+router.use(login)
 router.use(index)
 router.use('/ajax',ajax)
 router.use('/mobile',mobile)
