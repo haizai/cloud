bd69a96134b907998f296c3728c9875fd8ef81a2
haizai <haizainaive@gmail.com>
2017-01-30 22:54:59 +0800

    script src

---
+++ server/public/js/user/login.js
@@ -0,0 +1,47 @@
+jQuery(function(){
+  function login() {
+    $('.login-text').text('登入中...')
+    $.ajax({
+      url: 'ajax/login',
+      data: {
+        account: $('#login-account').val(),
+        password: $('#login-password').val()
+      },
+      success: function(doc){
+        switch (doc.state) {
+          case 1:
+            $('.login-text').text('登入成功！ 2秒后自动转跳')
+            setTimeout(function(){
+              window.location = 'user'
+            }, 2000)
+            break;
+          case 1001:
+            $('.login-text').text('用户名为空')
+            break;
+          case 1002:
+            $('.login-text').text('密码为空')
+            break;
+          case 1003:
+            $('.login-text').text('用户名不存在')
+            break;
+          case 1004:
+            $('.login-text').text('密码错误')
+            break;
+          case 1005:
+            $('.login-text').text('数据库错误')
+            break;
+          default:
+            $('.login-text').text('未知错误')
+            break;
+        }
+      }
+    })
+  }
+  $('#login-submit').on('click', login)
+  $('#login-password').on('keydown', function(e){
+    if (e.keyCode == 13) {
+      $(this).blur()
+      login()
+    }
+  })
+})
---
+++ server/public/js/user/user.js
@@ -0,0 +1,22 @@
+jQuery(function($){
+  $('#user-logoff').on('click',function(){
+    $.get('ajax/logoff',function(){
+      window.location.pathname = 'login'
+    })
+  })
+  $('#user-sign-span').on('click',function(){
+    $(this).hide().next().show().val($(this).text()).focus().select()
+  })
+  $('#user-sign-input').on('keydown', function(e){
+    if (e.keyCode == 13) $(this).blur()
+  })
+  $('#user-sign-input').on('blur', function(e){
+    var val = $(this).val().trim()
+    if (val !== ''){
+      $(this).hide().prev().show().text(val)
+      $.post('ajax/changeSign',{sign: val})
+    } else {
+      $(this).hide().prev().show().text('修改失败，请输入非空签名！')
+    }
+  })
+})
