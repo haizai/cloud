1b37518eaa43fb2ba1899e9c0720b30f23a519af
haizai <haizainaive@gmail.com>
2017-01-31 09:30:09 +0800

    add user.ejs-changeSex

--- server/public/js/user/user.js
+++ server/public/js/user/user.js
@@ -4,19 +4,25 @@ jQuery(function($){
       window.location.pathname = 'login'
     })
   })
+  var lastSignVal = ''
   $('#user-sign-span').on('click',function(){
-    $(this).hide().next().show().val($(this).text()).focus().select()
+    lastSignVal = $(this).text()
+    $(this).hide().next().show().val(lastSignVal).focus().select()
   })
   $('#user-sign-input').on('keydown', function(e){
     if (e.keyCode == 13) $(this).blur()
   })
   $('#user-sign-input').on('blur', function(e){
     var val = $(this).val().trim()
+    if (val === lastSignVal) return
     if (val !== ''){
       $(this).hide().prev().show().text(val)
       $.post('ajax/user/changeSign',{sign: val})
     } else {
       $(this).hide().prev().show().text('修改失败，请输入非空签名！')
     }
+  })
+  $('#user-sex').on('change',function (e) {
+    $.post('ajax/user/changeSex',{sex:$(this).val()})
   })
 })
