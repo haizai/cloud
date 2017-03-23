107066c1153ff915577c588c43d6b339e6e858c3
haizai <haizainaive@gmail.com>
2017-03-23 17:28:53 +0800

    解决 在socket.io中获取(解析)ajax的seesion(cookie)

--- server/app.js
+++ server/app.js
@@ -1,44 +1,47 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var formidable = require('formidable');
-var session = require('express-session')
-
-
-var routes = require('./routes/routes');
-
 var app = express();
 
-// view engine setup
+var path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
+// var formidable = require('formidable');
+//var favicon = require('serve-favicon');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-if (process.env.NODE_ENV == "dev") app.use(logger('dev'));
+
+
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
+var cookieParser = require('cookie-parser');
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 // express-session
+var session = require('express-session')
 app.set('trust proxy', 1)
-let sess = {
+let sessionMiddleware = session({
   secret: 'keyboard cat',
   cookie: {}
-}
-app.use(session(sess))
+})
+app.use(sessionMiddleware)
 
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+var routes = require('./routes/routes');
 app.use('/', routes);
 
 
 if (process.env.NODE_ENV == "dev") {
 
+  var logger = require('morgan');
+  app.use(logger('dev'))
+
+  //热更新
   var webpack = require('webpack')
   var webpackDevMiddleware = require('webpack-dev-middleware')
   var webpackHotMiddleware = require('webpack-hot-middleware')
@@ -74,14 +77,14 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
 
-module.exports = app;
+module.exports = {
+  app,
+  sessionMiddleware
+};
--- server/bin/server.js
+++ server/bin/server.js
@@ -1,9 +1,35 @@
-var app = require('../app');
+var app = require('../app').app;
 var port = require('./port');
-var http = require('http');
-
 app.set('port', port);
 
-var server = http.createServer(app);
+var http = require('http');
+var server = http.Server(app);
+var io = require('socket.io')(server)
+var sessionMiddleware = require('../app').sessionMiddleware;
+
+io.use(function(socket, next) {
+  sessionMiddleware(socket.request, socket.request.res, next);
+});
+
+
+io.of('gomoku').on('connection', function (socket) {
+  console.log('gomoku connection')
+  socket.on('log',()=>{
+    console.log(
+      'socket.request.session:\n\n',
+      socket.request.session
+    );
+  })
+  socket.on('join',()=>{
+    socket.join('roomtest')
+  })
+
+  socket.on('seesion', ()=>{
+    console.log('seesion:\n\n',seesion);
+  })
+
+});
+
+
 
 module.exports = server;
