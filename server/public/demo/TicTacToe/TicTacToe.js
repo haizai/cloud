e6d4821ca6f2523ed47ec93447f577b18278deeb
haizai <haizainaive@gmail.com>
2017-06-19 16:33:00 +0800

    优化分离 井字棋游戏

--- server/public/demo/TicTacToe/TicTacToe.js
+++ server/public/demo/TicTacToe/TicTacToe.js
@@ -1,96 +1,108 @@
-var outerHeight = document.body.offsetHeight
-var outerWidth = document.body.offsetWidth
-
-document.getElementById('board').style.width = outerWidth + 'px'
-document.getElementById('board').style.height = outerWidth + 'px'
+window.onload = function(){
 
 
-var Mission = function(){
-  this._cells = {}
-  this.indexs = [
-    ['0_0','0_1','0_2'],
-    ['1_0','1_1','1_2'],
-    ['2_0','2_1','2_2'],
-    ['0_0','1_0','2_0'],
-    ['0_1','1_1','2_1'],
-    ['0_2','1_2','2_2'],
-    ['0_0','1_1','2_2'],
-    ['0_2','1_1','2_0']
-  ]
-  this.nowPlayer = 1,
-  this.stage = 'playing'
-  this.el = {
-    board: document.getElementById('board')
+  function lengthInresize(){
+    var outerHeight = document.body.offsetHeight
+    var outerWidth = document.body.offsetWidth
+    var unit = Math.min(outerHeight,outerWidth,600)
+    document.getElementById('board').style.width = unit + 'px'
+    document.getElementById('board').style.height = unit + 'px'
   }
-  this.init()
-  this.bindEvent()
-}
+  lengthInresize()
 
-var Cell = function(x,y,mission){
-  this.x = x
-  this.y = y
-  this.player = null
-  this.mission = mission
-  this.dom = document.getElementById(x+'_'+y)
-}
-Cell.prototype = {
-  render: function(){
-    this.dom.classList.add('_'+this.player)
-  }
-}
+  var nowPlayer = 1
+  var myPlayer = null
+  var stage = 'wait'
+  var scoket 
 
-Mission.prototype = {
-  init: function(){
-      for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-          let cell = new Cell(i,j,this)
-          this._cells[i+'_'+j] = cell
-        }
-      }
-  },
-  getCell: function(x,y){
-    return this._cells[x+'_'+y]
-  },
-  move: function(player,x,y){
-    if (this.stage == 'playing' && player == this.nowPlayer) {
-      var cell = this.getCell(x,y)
-      if(cell.player === null){
-        cell.player = player
-        cell.render()
-      }
-      this.nowPlayer == 1 ? this.nowPlayer = 2 : this.nowPlayer = 1;
-      this.test()
-    }    
-  },
-  test: function(){
-    var cells = this._cells
-    var indexs = this.indexs
-    for (var i = 0; i <indexs.length; i++) {
-      var arr = []
-      indexs[i].forEach(function(j){
-        arr.push(cells[j].player)
-      })
-      if (arr[0] !== null && arr[0] === arr[1] && arr[1] === arr[2] ) {
-        console.log('end',indexs[i])
-        this.stage = 'end'
-      }
-    }
-  },
-  bindEvent: function(){
-    var self = this
-    this.el.board.addEventListener('click', function(e){
-      document.getElementById('tip').innerHTML = e.target.id +','+ e.target.className
-      if(e.target.classList.contains('cell')){
-        var arr = e.target.id.split('_')
-        var x = +arr[0]
-        var y = +arr[1]
-        var cell = self.getCell(x,y)
-        cell.mission.move(cell.mission.nowPlayer,x,y)
-      }
+   function reset(){
+    nowPlayer = 1
+    myPlayer = null
+    stage = 'wait'
+    document.getElementById('tip').innerHTML = 'reset'
+    Array.prototype.slice.apply(document.getElementsByClassName('cell')).forEach(function(dom){
+      dom.className = 'cell'
     })
+   }
+
+
+
+  scokeConnect()
+  function scokeConnect(){
+    if (scoket) {
+      socket.close()
+    }
+    socket = io('http://localhost/ttt');
+    // var socket = io('/ttt');
+    socket.on('connect', function(){
+      // console.log(socket.id);
+    });
+
+    socket.on('info', function (data) {
+      console.info('info',data);
+      switch (data) {
+        case 'wait':
+          document.getElementById('tip').innerHTML = '等待他人中...'
+          break;
+        case 'disconnect':
+          alert('对方离开或重置游戏')
+          reset()
+          scokeConnect()
+          break;
+      }
+    });
+
+    socket.on('start', function (p1,p2,room) {
+      if (p1 === socket.id) {
+        myPlayer = 1
+      } else if (p2 === socket.id) {
+        myPlayer = 2
+      }
+      stage = 'playing'
+      var myPlayerChinese = myPlayer == 1 ? '你先落子' : '等待对方落子';
+      document.getElementById('tip').innerHTML = '游戏开始，'+myPlayerChinese
+      document.getElementById('room').innerHTML = '房间 '+room
+    });
+
+    socket.on('move', function (player,x,y) {
+      document.getElementById(x+'_'+y).classList.add('_'+player)
+      nowPlayer == 1 ? nowPlayer = 2 : nowPlayer = 1;
+      if (nowPlayer === myPlayer) {
+        document.getElementById('tip').innerHTML = '请落子'
+      } else {
+        document.getElementById('tip').innerHTML = '等待对方落子'
+      }
+    });
+    socket.on('end', function (player,x,y,winPlayer,ids) {
+      document.getElementById(x+'_'+y).classList.add('_'+player)
+      ids.forEach(function(id){
+        document.getElementById(id).classList.add('win')
+      })
+      if (winPlayer === myPlayer) {
+        document.getElementById('tip').innerHTML = '你赢了，可以下方点击「重置游戏」'
+      } else {
+        document.getElementById('tip').innerHTML = '你输了，可以下方点击「重置游戏」'
+      }
+    });
+
   }
+
+  document.addEventListener('click', function(e){
+    if (stage!=='playing') return 
+    if (myPlayer!==nowPlayer) return
+    var tg = e.target
+    if(tg.classList.contains('cell') && !tg.classList.contains('_1')&& !tg.classList.contains('_2')){
+      var arr = tg.id.split('_')
+      var x = +arr[0]
+      var y = +arr[1]
+      socket.emit('tryMove',x,y)
+    }
+  })
+  window.addEventListener('resize',function(e){
+    lengthInresize()
+  })
+  document.getElementById('reset').addEventListener('click', function(e){
+    location.reload(true)
+  })
+    
 }
-
-
-
-var mission = new Mission()
